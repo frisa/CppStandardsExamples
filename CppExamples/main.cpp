adcdd3edbd8f7145f32a0e3eee86a9119c0b01b7
Jan Fridrichovsky <fridrichovsky@gmail.com>
2021-05-01 19:37:54 +0200

    Ex#1 Padding

--- CppExamples/main.cpp
+++ CppExamples/main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+class A{
+  public:
+    int* px;
+    int* x;
+};
+
 int main()
 {
     cout << "Generic C++ Examples" << endl;
+    cout << "size of A is: " << sizeof(A) << endl;
+    cout << "size of *int: " << sizeof(int*) << endl;
+    cout << " size of int: " << sizeof(int) << endl;
     return 0;
 }
