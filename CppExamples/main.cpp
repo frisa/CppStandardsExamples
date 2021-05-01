7b6f0e81f3474ce70f264020c8c6ca69266ae448
Jan Fridrichovsky <fridrichovsky@gmail.com>
2021-05-01 19:48:24 +0200

    Data Types sizes

--- CppExamples/main.cpp
+++ CppExamples/main.cpp
@@ -14,5 +14,14 @@ int main()
     cout << "size of A is: " << sizeof(A) << endl;
     cout << "size of *int: " << sizeof(int*) << endl;
     cout << " size of int: " << sizeof(int) << endl;
+
+    cout << "sizes of the data types (bytes):" << endl;
+    cout << "char size:" << sizeof(char) << endl;
+    cout << "short size:" << sizeof(short) << endl;
+    cout << "int size:" << sizeof(int) << endl;
+    cout << "long size:" << sizeof(long) << endl;
+    cout << "bool size:" << sizeof(bool) << endl;
+    cout << "float size:" << sizeof(float) << endl;
+    cout << "double size:" << sizeof(double) << endl;
     return 0;
 }
